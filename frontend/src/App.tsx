4b0b98d605435e087ab696eabfd9c9b482d5cb25
NiklasSchibel <95278041+NiklasSchibel@users.noreply.github.com>
2022-04-12 21:49:06 +0200

    theme added, favicon missing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import Blog from "./BlogTheme/Blog";
 
 
 export default function App() {
     return (
-        <div>
-             <h1>Hi World!!</h1>
-        </div>
+        <Blog/>
     );
 }
