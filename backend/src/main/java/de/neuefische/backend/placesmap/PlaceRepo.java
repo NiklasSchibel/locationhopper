9efc04f69ea6a7a6ab13ff35d6a6ed387ec2f3cd
NiklasSchibel <95278041+NiklasSchibel@users.noreply.github.com>
2022-04-16 14:34:49 +0200

    undo mongoDB to go live

--- backend/src/main/java/de/neuefische/backend/placesmap/PlaceRepo.java
+++ backend/src/main/java/de/neuefische/backend/placesmap/PlaceRepo.java
@@ -1,14 +1,14 @@
 package de.neuefische.backend.placesmap;
 
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface PlaceRepo extends MongoRepository<Place, String> {
-
-    List<Place> findAllByUsername(String username);
-
-}
+//@Repository
+//public interface PlaceRepo extends MongoRepository<Place, String> {
+//
+//    List<Place> findAllByUsername(String username);
+//
+//}
